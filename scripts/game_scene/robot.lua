c21d617b17ecf5a7b325a1dc4eedca4f8603477f
Gabriel Naves da Silva <gabri.navess@gmail.com>
2018-02-10 16:28:20 -0200

    Basic horizontal movement implemented

--- scripts/game_scene/robot.lua
+++ scripts/game_scene/robot.lua
@@ -2,12 +2,14 @@ local robot = {}
 
 -- Image data
 robot.img = love.graphics.newImage('assets/robot.png')
-robot.num_frames = 3
+robot.num_frames = 2
 robot.frame_width = robot.img:getWidth() / robot.num_frames
 robot.frame_height = robot.img:getHeight()
-robot.body_quad = love.graphics.newQuad(0, 0, robot.frame_width, robot.frame_height, robot.img:getWidth(), robot.img:getHeight())
-robot.arm_quad = love.graphics.newQuad(robot.frame_width, 0, robot.frame_width, robot.frame_height, robot.img:getWidth(), robot.img:getHeight())
-robot.head_quad = love.graphics.newQuad(robot.frame_width*2, 0, robot.frame_width, robot.frame_height, robot.img:getWidth(), robot.img:getHeight())
+robot.frames = {
+    love.graphics.newQuad(0, 0, robot.frame_width, robot.frame_height, robot.img:getWidth(), robot.img:getHeight()),
+    love.graphics.newQuad(robot.frame_width, 0, robot.frame_width, robot.frame_height, robot.img:getWidth(), robot.img:getHeight()),
+}
+robot.flip = false
 
 -- Motion data
 robot.rect = geometry.makeRect(measure.screen_width/2, measure.screen_height-2, robot.frame_width, robot.frame_height, 0.5, 1)
@@ -21,6 +23,7 @@ robot.mov_decay = 0.75
 function robot:update(dt)
     self:updateVelocity(dt)
     self:updatePosition(dt)
+    self:updateFlip(dt)
 end
 
 function robot:updateVelocity(dt)
@@ -50,12 +53,20 @@ function robot:updatePosition(dt)
     end
 end
 
+function robot:updateFlip(dt)
+    if self.velocity.x > 0 then
+        self.flip = false
+    elseif self.velocity.x < 0 then
+        self.flip = true
+    end
+end
+
 function robot:draw()
     local draw_x = self.rect.x - self.rect.width*self.rect.pivotX
     local draw_y = self.rect.y - self.rect.height*self.rect.pivotY
-    love.graphics.draw(self.img, self.body_quad, draw_x, draw_y)
-    love.graphics.draw(self.img, self.arm_quad, draw_x, draw_y)
-    love.graphics.draw(self.img, self.head_quad, draw_x, draw_y)
+    local frame = 1
+    if self.flip then frame = 2 end
+    love.graphics.draw(self.img, self.frames[frame], draw_x, draw_y)
 end
 
 return robot
