6e7aec29ce7550f155bb86dbc48e2cec87a87487
Gabriel Naves da Silva <gabri.navess@gmail.com>
2018-02-11 11:09:13 -0200

    Implemented menu info and player amount settings

--- scripts/scenes/menu_scene.lua
+++ scripts/scenes/menu_scene.lua
@@ -13,13 +13,33 @@ menu_scene.text_info = {
         text = "a game by Gabriel Naves",
         height = 100,
         scale = 1,
+    },
+    {
+        text = "Player amount:",
+        height = measure.screen_height / 2 - 20,
+        scale = 1,
     }
 }
 
-function menu_scene:load()
-end
+menu_scene.right_arrow = still_image.new('right_arrow.png', measure.screen_width/2 + 50, measure.screen_height/2+40, 0.5, 0.5)
+menu_scene.left_arrow = still_image.new('left_arrow.png', measure.screen_width/2 - 50, measure.screen_height/2+40, 0.5, 0.5)
+menu_scene.right_arrow_rect = geometry.makeRect(menu_scene.right_arrow.x, menu_scene.right_arrow.y,
+                                                menu_scene.right_arrow.width, menu_scene.right_arrow.height,
+                                                menu_scene.right_arrow.pivotX, menu_scene.right_arrow.pivotY)
+menu_scene.left_arrow_rect = geometry.makeRect(menu_scene.left_arrow.x, menu_scene.left_arrow.y,
+                                               menu_scene.left_arrow.width, menu_scene.left_arrow.height,
+                                               menu_scene.left_arrow.pivotX, menu_scene.left_arrow.pivotY)
+
+menu_scene.player_amount = 2
 
 function menu_scene:update(dt)
+    if mouse.mouseButtonDown then
+        if geometry.isPointInRect(geometry.makePoint(mouse.mouseX, mouse.mouseY), self.right_arrow_rect) then
+            self.player_amount = gamemath.clamp(self.player_amount+1, 4, 2)
+        elseif geometry.isPointInRect(geometry.makePoint(mouse.mouseX, mouse.mouseY), self.left_arrow_rect) then
+            self.player_amount = gamemath.clamp(self.player_amount-1, 4, 2)
+        end
+    end
 end
 
 function menu_scene:lateUpdate(dt)
@@ -31,11 +51,18 @@ function menu_scene:draw()
         love.graphics.print({self.text_color, info.text},
                             math.floor(draw_x), info.height, 0, info.scale, info.scale)
     end
+    self.right_arrow:draw()
+    self.left_arrow:draw()
+    self:drawPlayerAmount()
+end
+
+function menu_scene:drawPlayerAmount()
+    local draw_x = measure.screen_width/2 - font:getWidth(tostring(self.player_amount))/2
+        love.graphics.print({self.text_color, tostring(self.player_amount)},
+                            math.floor(draw_x), measure.screen_height/2+20, 0, 2, 2)
 end
 
 function menu_scene:restart()
 end
 
-
-menu_scene:load()
 return menu_scene
